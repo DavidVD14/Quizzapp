bfe56038a97d1f8834b08ab2c5dadf52fcc2108a
DavidVD14 <115690857+DavidVD14@users.noreply.github.com>
2024-05-09 19:53:14 +0200

    Add files via upload

---
+++ src/Quiz.java
@@ -0,0 +1,145 @@
+public class Quiz{
+
+	//Properties
+    char Question1, Question2, Question3, Question4, Question5;
+
+
+	//Method
+	public void showCategory1(){
+		System.out.println("1. Who created the first plane  which flew?");
+		System.out.println("A)Leroy Grumman\n  B)Harry George Hawker\n  C)Wilbur and Orville wright\n  D)Allan and Malcolm Loughead)\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("2. What is the fastest aircraft in the world?");
+		System.out.println("A)F-15 Strike Eagle\n  B)SR-71 Blackbird\n  C)F-22 Raptor\n  D)Mig 21\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("3. What aircraft can carry the most passengers?");
+		System.out.println("A)Boeing 747\n  B)Airbus A321\n  C)An-225\n  D)Airbus A380\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("4. The______ is the biggest aircraft in the world.	");
+		System.out.println("A)Airbus A380\n  B)C-130  Hercules\n  C)An-225\n  D)C-5 Galaxy\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("5. The_____ Enables the aircraft to fly by itself.");
+		System.out.println("A)Auto navigator\n  B)Auto Plane\n  C)Automatic flying\n  D)AutoPilot\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("6. which was the first aircraft to break the sound barrier?");
+		System.out.println("A)SR-71 Blackbird\n  B)Airbus A380\n  C)B2-bomber\n  D)Bell X1\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("7. For how long did the first aircraft fly for?");
+		System.out.println("A)2 minutes\n  B)12 seconds\n C)10 minutes\n  D)1 hour\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("8. When did air Malta been founded?");
+		System.out.println("A)1973\n  B)1960\n C)1998\n D)1983\n");
+		Question1 = Keyboard.readChar();
+
+	    System.out.println("9. The Lockheed C-130 transport plane is also known as ... ?");
+		System.out.println("A)Zeus\n  B)Hercules\n C)Atlas\n  D)Omega\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("10. The Boeing 747 first entered commercial service in ... ?");
+		System.out.println("A)1950\n  B)1960\n  C)1970\n  D)1980\n");
+		Question1 = Keyboard.readChar();
+
+	}
+
+		//Method2
+		public void showCategory2(){
+
+		System.out.println("1.Who won the fifa world cup of 2014?");
+		System.out.println("A)Germany\n B)Italy\n C)Spain\n D)France\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("2.What is the name of the famous 500 mile car race?");
+		System.out.println("A)Indianapolis 500\n B)Monza C)24 Hours of Nurburgring\n D)24 Hours of Le Mans\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("3.Where did the 2016 Olympics take place?");
+		System.out.println("A)London\n B)Brasil\n C)Germany\n D)Argentina\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("4.Which European team won the most champions league cup?");
+		System.out.println("A)Chelsea\n B)Manchester united\n  C)real Madrid\n D)Juventus\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("5.	What is the world record for the 100 metre sprint?");
+		System.out.println("A)12s\n B)11s\n C)6.72s\n D)9.58s\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("6.	What sport does use a puck on ice?");
+		System.out.println("A)Ice Hockey\n B)curling\n C)Fencing\n D)Ice skating\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("7.	What is the terminal velocity while skydiving?");
+		System.out.println("A)100kmph\n B)90kmph\n C)160kmph\n D)195kmph\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("8.	Tiger woods plays which sport?");
+		System.out.println("A)Football\n B)Golf\n C)Skiing\n D)Tennis\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("What sport used the term ‘home run’ long before baseball? ");
+		System.out.println("A)Cricket\n B)Rugby\n C)Football\n D)Handball\n");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("9.	Which is the only country to have played in each and every World Cup? ");
+		System.out.println("A)Brasil\n B)Germany\n C)Netherlands\n D)USA\n");
+		Question1 = Keyboard.readChar();
+
+		}
+
+	//Method3
+	public void showCategory3(){
+
+		System.out.println("How many oscars did Leonardio di caprio won?");
+		System.out.println("A)0\n  B)1\n  C)4\n  D)2\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("who voices the character Dory in Finding Dory?");
+		System.out.println("A)Ellen degeneres\n B)Owen Wilson\n C)Jennifer Lawrence\n D)Kristen Bell\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("who is the name of the main actor in the Iron man Franchise?");
+		System.out.println("A)Chris Evans\n B)Tobey Maguire\n C)Robert downey JR\n  D)Stan Lee\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("which company is the developer of Battlfield 1");
+		System.out.println("A)DICE\n  B)EA\n  C)Activision\n D)Rockstar\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("what is the name of the current generatio of the playstation console?");
+		System.out.println("A)Playstation vita\n  B)Playstation 4\n  C)Playstation 3\n  D)Playstation portable\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("who starres as Draco Malfoy in Harry Potter?");
+		System.out.println("A)Matthew Lewis\n  B)Daniel Radcliffe\n  C)Ron Weasley\n  D)Tom Felton\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("In what year did the xbox 360 Launched?");
+		System.out.println("A)2005\n  B)2007\n  C)2010\n  D)2004\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("In the Hobbit's Movies what is the name of the wizrd which helpt the dwarfs?");
+		System.out.println("A)Dumbledor\n  B)Gandalf\n  C)Merlin\n  D)Harry Potter\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("n what year did doc and mart travel in back to the future 2?");
+		System.out.println("A)2015\n  B)2017\n  C)2050\n  D)2033\n ");
+		Question1 = Keyboard.readChar();
+
+		System.out.println("Who was the famous which starred as princess lea in star wars?");
+		System.out.println("A)jennifer lawrence\n  B)Dolly parton\n  C)Carrie Fisher\n  D)Emma watson\n ");
+		Question1 = Keyboard.readChar();
+	}
+
+
+
+
+
+
+}
---
+++ src/QuizApp.java
@@ -0,0 +1,9 @@
+
+public class QuizApp {
+
+    public static void main(String[] args) {
+	Quiz copy = new Quiz();
+	copy.showCategory1();
+
+    }
+}
